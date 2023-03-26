ab7887082a396e4b6f11c32cb94b6b2371aa70e9
bielansky <remigiusz.bielansky@gmail.com>
2023-03-26 21:39:01 +0200

    Displays all the posts and comments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => setPosts(data));
+  }, []);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+        .then(response => response.json())
+        .then(data => setComments(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Posts</h1>
+        <ol>
+          {posts.map(post => (
+              <li key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+                <ol>
+                  {comments
+                      .filter(comment => comment.postId === post.id)
+                      .map(comment => (
+                          <li key={comment.id}>
+                            <h5>{comment.name}</h5>
+                            <p>{comment.body}</p>
+                          </li>
+                      ))}
+                </ol>
+              </li>
+          ))}
+        </ol>
+      </div>
   );
 }
 
