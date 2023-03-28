fb7cb8ff5a3ffcc5e4a55d894168c18fea83cfa9
bielansky <remigiusz.bielansky@gmail.com>
2023-03-28 11:47:12 +0200

    Page loading and comments loading patch

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     const [posts, setPosts] = useState([]);
     const [comments, setComments] = useState([]);
     const [visibleComments, setVisibleComments] = useState({});
+    const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -25,47 +26,63 @@ function App() {
             .then(data => setComments(data));
     }, []);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            if (
+                window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+                currentPage * 3 < posts.length
+            ) {
+                setCurrentPage(currentPage + 1);
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [currentPage, posts]);
+
     const showMoreComments = (postId) => {
         setVisibleComments({
             ...visibleComments,
             [postId]: (visibleComments[postId] || 0) + 3,
         });
-    }
+    };
 
-    const getCommentsToShow = (postId) =>
-        comments
-            .filter((comment) => comment.postId === postId)
-            .slice(0, visibleComments[postId] || 0);
+    const getCommentsToShow = (postId) => {
+        const postComments = comments.filter((comment) => comment.postId === postId);
+        return postComments.slice(0, visibleComments[postId] || 0);
+    };
 
+    const hasMoreComments = (postId) => {
+        const postComments = comments.filter((comment) => comment.postId === postId);
+        return getCommentsToShow(postId).length < postComments.length;
+    };
 
-  // const commentsToShow = comments.slice(0, visibleComments);
+    const paginatedPosts = posts.slice(0, currentPage * 3);
 
-  return (
-      <div>
-        <h1>Posts</h1>
-        <ol>
-          {posts.map(post => (
-              <li key={post.id}>
-                <h3>{post.title}</h3>
-                <p>{post.body}</p>
-                <ol>
-                  {getCommentsToShow(post.id)
-                      // .filter(comment => comment.postId === post.id)
-                      .map(comment => (
-                          <li key={comment.id}>
-                            <h5>{comment.name}</h5>
-                            <p>{comment.body}</p>
-                          </li>
-                      ))}
-                </ol>
-                  {getCommentsToShow(post.id).length < comments.length && (
-                  <button onClick={() => showMoreComments(post.id)}>Load more</button>
-                  )}
-              </li>
-          ))}
-        </ol>
-      </div>
-  );
+    return (
+        <div>
+            <h1>Posts</h1>
+            <ol>
+                {paginatedPosts.map(post => (
+                    <li key={post.id}>
+                        <h3>{post.title}</h3>
+                        <p>{post.body}</p>
+                        <ol>
+                            {getCommentsToShow(post.id).map(comment => (
+                                <li key={comment.id}>
+                                    <h5>{comment.name}</h5>
+                                    <p>{comment.body}</p>
+                                </li>
+                            ))}
+                        </ol>
+                        {hasMoreComments(post.id) && (
+                            <button onClick={() => showMoreComments(post.id)}>Load more</button>
+                        )}
+                    </li>
+                ))}
+            </ol>
+        </div>
+    );
 }
 
 export default App;
