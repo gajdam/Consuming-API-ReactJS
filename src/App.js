cbb5efe336046eac67be9ea1814c3ad3e7dc2bfc
gajdam <gajam11@gmail.com>
2023-04-25 15:41:26 +0200

    theme update

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import Posts from './Posts';
 import Albums from "./Albums";
 import {createContext} from "react";
 
-
-
 export const ThemeContext = createContext(null);
 const App = () => {
-
     const [postsOrAlbums, setPostsOrAlbums] = useState(true);
+    const [bgTheme, setBgTheme] = useState(null);
+    const [colorTheme, setColorTheme] = useState(null);
+    const [btnTheme, setBtnTheme] = useState(null);
+    const [btnHoverTheme, setBtnHoverTheme] = useState(null);
 
-    const [theme, setTheme] = useState("light");
-
-    const toggleTheme = () => setTheme((curr) => (curr === "light" ? "dark" : "light"));
+    const toggleTheme = () => {
+        const root = document.documentElement;
+        const newBgTheme = bgTheme === "light" ? "dark" : "light";
+        const newColorTheme = bgTheme === "light" ? "dark" : "light";
+        const newBtnTheme = bgTheme === "light" ? "dark" : "light";
+        const newBtnHoverTheme = bgTheme === "light" ? "dark" : "light";
+        root.style.setProperty('--bg-color', newBgTheme === "light" ? 'grey' : 'black');
+        root.style.setProperty('--txt-color', newColorTheme === "light" ? 'black' : 'white');
+        root.style.setProperty('--btn-color', newBtnTheme === "light" ? '#41658A' : 'pink');
+        root.style.setProperty('--btn-hover', newBtnHoverTheme === "light" ? 'grey' : 'red');
+        setBgTheme(newBgTheme);
+        setColorTheme(newColorTheme);
+        setBtnTheme(newBtnTheme);
+        setBtnHoverTheme(newBtnHoverTheme);
+    };
 
     return (
-        <ThemeContext.Provider value={{theme, toggleTheme}}>
-        <div className="App" id={theme}>
+        <div className="App">
             <h1>Consuming json placeholder API</h1>
             <button onClick={toggleTheme}>klik</button>
             <div>
@@ -28,7 +40,6 @@ const App = () => {
                 : <Albums/>
             }
         </div>
-        </ThemeContext.Provider>
     );
 };
 
